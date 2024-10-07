fcf85064e27ecd171814e4e22f134270d8282edf
Documenter.jl <documenter@juliadocs.github.io>
2024-10-07 09:46:49 +0000

    build based on e6467b8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SteadyWaves","category":"page"},{"location":"#SteadyWaves","page":"Home","title":"SteadyWaves","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SteadyWaves.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SteadyWaves]","category":"page"}]
+}
