4da303fc1a414f0fb4249c1c9eb6532cb452887d
mcpaprota <mapap@ibwpan.gda.pl>
2024-10-08 12:39:50 +0200

    module description added

--- src/SteadyWaves.jl
+++ src/SteadyWaves.jl
@@ -1,3 +1,8 @@
+"""
+SteadyWaves is an implementation of Rienecker and Fenton (1981)
+Fourier Approximation Method to steady, periodic, nonlinear waves
+propagating in water of constant depth.
+"""
 module SteadyWaves
 
 using NonlinearSolve
