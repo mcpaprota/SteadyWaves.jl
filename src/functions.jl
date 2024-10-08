6682fd0f29bddfbc29227c845e4fd64e15fdae5e
mcpaprota <mapap@ibwpan.gda.pl>
2024-10-08 11:59:52 +0200

    method implemented, basic tests passed

--- src/functions.jl
+++ src/functions.jl
@@ -1,16 +1,19 @@
 """
-    fourier_approx(d, H, L; cc=1, N=10, M=1)
+    fourier_approx(d, H, P; pc=1, cc=1, N=10, M=1, g=9.81)
 
-Approximate wave solution `u` using Fourier Approximation Method.
+Approximate solution `u` of a steady wave of height `H` and length `L`
+propagating in water of depth `d` using Fourier Approximation Method.
 
 ...
 # Arguments
 - `d`: water depth (m)
 - `H`: wave height (m)
-- `L`: wavelength (m)
-- `cc`: current criterion 1 - Stokes; 2 - euler
-- `N`: number of solution eigenvalues
-- `M`: number of height
+- `P`: wave parameter - length `L` (m) or period `T` (s)
+- `pc`: parameter criterion; `pc=1` - length (default), `pc=2` - period
+- `cc`: current criterion; `cc=1` - Stokes (default), `cc=2` - Euler
+- `N`: number of solution eigenvalues, defaults to `N=10`
+- `M`: number of height steps, defaults to `M=1`
+- `g`: gravity acceleration (m/s^2), defaults to `g=9.81`
 
 # Output (nondimensional)
 - `u[1:N+1]`: free surface elevation *kη*
@@ -21,10 +24,14 @@ Approximate wave solution `u` using Fourier Approximation Method.
 - `u[2N+5]`: Bernoulli constant *rk/g*
 - `u[2N+6]`: mean flow velocity *Ū√(k/g)*
 """
-function fourier_approx(d, H, L; cc=1, N=10, M=1)
-    u = init_conditions(d, H, L, N, M)
+function fourier_approx(d, H, P; pc=1, cc=1, N=10, M=1, g=9.81)
+    u = init_conditions(d, H, P, pc, N, M)
     for m in 1:M
-        params = [L / d, H / d * m / M, cc]
+        if pc==1
+            params = [P / d, H / d * m / M, pc, cc]
+        else
+            params = [P * sqrt(g / d), H / d * m / M, pc, cc]
+        end
         problem = NonlinearProblem(f, u, params)
         solution = solve(problem, RobustMultiNewton())
         u[:] = solution.u
@@ -32,8 +39,32 @@ function fourier_approx(d, H, L; cc=1, N=10, M=1)
     return u
 end
 
-function init_conditions(d, H, L, N, M)
-    k = 2π / L # wave number (rad/m)
+"""
+    init_conditions(d, H, L, N, M)
+
+Calculate initial conditions `u0` of a steady wave of height `H` and length `L`
+propagatin in water of depth `d` using linear wave theory.
+
+...
+# Arguments
+- `d`: water depth (m)
+- `H`: wave height (m)
+- `P`: wave parameter - length `L` (m) or period `T` (s)
+- `pc`: parameter criterion; `pc=1` - length, `pc=2` - period
+- `N`: number of solution eigenvalues
+- `M`: number of height steps
+
+# Output (nondimensional)
+- `u[1:N+1]`: free surface elevation *kη*
+- `u[N+2:2N+1]`: stream function coefficients *B*
+- `u[2N+2]`: wave celerity *c√(k/g)*
+- `u[2N+3]`: mean water depth *kη̄*
+- `u[2N+4]`: volume flux due to waves *q√(k³/g)*
+- `u[2N+5]`: Bernoulli constant *rk/g*
+- `u[2N+6]`: mean flow velocity *Ū√(k/g)*
+"""
+function init_conditions(d, H, P, pc, N, M)
+    pc == 1 ? k = 2π / P : k = disp_rel(d, 2π / P) # wave number (rad/s)
     u0 = zeros(2N + 6)
     u0[1:N+1] = @. k * d + 1 / 2 * k * H / M * cos((0:N) * π / N) # kη
     u0[N+2:2N+1] = [k * H / M / 2 / √tanh(k * d); zeros(N - 1)] # B
@@ -45,18 +76,49 @@ function init_conditions(d, H, L, N, M)
     return u0
 end
 
+"""
+    f(du, u, p)
+
+Define nonlinear system `f(u) = 0` with parameters `p`.
+
+"""
 function f(du, u, p)
     N = (length(u) - 6) ÷ 2
     for m in 0:N
-        Σ₁ = sum([u[N+1+j] * sinh(j * u[m+1]) / cosh(j * u[2N+3]) * cos(j * m * π / N) for j in 1:N])
+        Σ₁ = sum([u[N+1+j] * sinh(j * u[m+1]) / cosh(j * u[2N+3]) * cos(j * m * π / N)
+                  for j in 1:N])
         du[m+1] = Σ₁ - u[2N+6] * (u[m+1] - u[2N+3]) - u[2N+4]
-        Σ₂ = sum([j * u[N+1+j] * cosh(j * u[m+1]) / cosh(j * u[2N+3]) * cos(j * m * π / N) for j in 1:N])
-        Σ₃ = sum([j * u[N+1+j] * sinh(j * u[m+1]) / cosh(j * u[2N+3]) * sin(j * m * π / N) for j in 1:N])
+        Σ₂ = sum([j * u[N+1+j] * cosh(j * u[m+1]) / cosh(j * u[2N+3]) * cos(j * m * π / N)
+                  for j in 1:N])
+        Σ₃ = sum([j * u[N+1+j] * sinh(j * u[m+1]) / cosh(j * u[2N+3]) * sin(j * m * π / N)
+                  for j in 1:N])
         du[N+1+m+1] = (-u[2N+6] + Σ₂)^2 / 2 + Σ₃^2 / 2 + u[m+1] - u[2N+3] - u[2N+5]
     end
     du[2N+3] = ((u[1] + u[N+1]) / 2 + sum(u[2:N])) / N - u[2N+3]
     du[2N+4] = u[1] - u[N+1] - u[2N+3] * p[2]
-    du[2N+5] = u[2N+3] - 2π / p[1]
-    p[3] == 1 ? du[2N+6] = u[2N+6] - u[2N+2] - u[2N+4] / u[2N+3] : du[2N+6] = u[2N+6] - u[2N+2]
+    if p[3] == 1
+        du[2N+5] = u[2N+3] - 2π / p[1]
+    else
+        du[2N+5] = u[2N+2] * p[1] * √u[2N+3] - 2π
+    end
+    if p[4] == 1
+        du[2N+6] = u[2N+6] - u[2N+2] - u[2N+4] / u[2N+3]
+    else
+        du[2N+6] = u[2N+6] - u[2N+2]
+    end
     return nothing
+end
+
+"""
+    disp_rel(d, ω, g)
+
+Calculate wavenumber `κ` based on depth `d`, angular wave frequency `ω`
+and gravitational acceleration `g` for given accuracy `ϵ`.
+"""
+function disp_rel(d, ω, g=9.81, ϵ=10^-12)
+    κ = κ₀ = ω^2 / g # initial guess
+    while max(abs(κ * tanh(κ * d) - κ₀)) > ϵ
+        κ = κ₀ / tanh(κ * d)
+    end
+    return κ
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@ using SteadyWaves
 using Test
 
 @testset "SteadyWaves.jl" begin
-    u = fourier_approx(1, 0.1, 1; cc = 2)
-    @test u[22] == u[26]
+    # no mass transport in a flume
+    d, H, L, g = 1, 0.1, 1, 9.81 # depth, height, length, gravity acceleration
+    u1 = fourier_approx(d, H, L; cc = 2)
+    @test u1[22] ≈ u1[26] # c√(k/g) = Ū√(k/g)
+    # c = L/T
+    T = L/u1[22]*√(2π/L/g) # T = c/L
+    u2 = fourier_approx(1, 0.1, T; pc=2, cc = 2)
+    @test u1[22] ≈ u2[22]
 end
