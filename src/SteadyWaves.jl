8c2284ad92d95c1cf46b7a44e4e7958c7cbd4eae
pankogito <stasbieg@gmail.com>
2026-03-04 13:34:01 +0100

    STW-89 Linear Wave Number

--- src/SteadyWaves.jl
+++ src/SteadyWaves.jl
@@ -24,13 +24,13 @@ using .Output: wave_height, wavelength, wave_power, wave_period,
 include("nonlinear_system.jl")
 
 include("steady.jl")
-using .Steady: fourier_approx, wave_number
+using .Steady: fourier_approx, linear_wave_number
 
 include("shoaling.jl")
 using .Shoaling: topo_approx, fourier_approx!
 
 export fourier_approx,fourier_approx!, topo_approx, wave_number
-export wave_period, wavelength, wave_height
+export wave_period, wavelength, wave_height, linear_wave_number
 export dimensionless_vertical_velocity, vertical_velocity
 export dimensionless_horizontal_velocity, horizontal_velocity
 export CurrentCriterion, CC_EULER, CC_STOKES
