9b794c9a1489e077f93aca7560791b5592f6a44d
pankogito <stasbieg@gmail.com>
2026-05-19 08:56:20 +0200

    STW-147 Timedepended Elevation

--- src/output.jl
+++ src/output.jl
@@ -131,8 +131,17 @@ end
 """
     elevation(w::WaveStruct, kx)
 
+    elevation(w::WaveStruct, kx, t)
+    elevation(w::WaveStruct, kx, t, c)
+
 Calculate free surface elevation from solution `w`
 at coordinate `kx` or `x`.
+
+If only `x` is provided returns value from wave perspective.
+
+If `t` is provided returns value from static perspective.
+
+If `t, s` is provided returns value from an arbitrary perspective.
 
 If `w` is dimensional provide dimensional `x`.
 If not provide `k*x`.
@@ -142,11 +151,28 @@ function elevation(w::WaveStruct,kx)
     return w.eta.z(kx)
 end
 
+function elevation(w::WaveStruct,kx,t)
+    return elevation(w,kx+w.C*t)
+end
+
+function elevation(w::WaveStruct,kx,t,c)
+    return elevation(w,kx+c*t)
+end
+
 """
     surface_tension(w, kx)
 
+    surface_tension(w, kx, t)
+    surface_tension(w, kx, t, c)
+
 Calculate surface tension from solution `w`
 at coordinate `kx` or `x`.
+
+If only `x` is provided returns value from wave perspective.
+
+If `t` is provided returns value from static perspective.
+
+If `t, s` is provided returns value from an arbitrary perspective.
 
 If `w` is dimensional provide dimensional `x`.
 If not provide `k*x`.
@@ -158,6 +184,14 @@ function surface_tension(w,kx)
         w.eta.z.dz_dx_1(kx),
         w.eta.z.dz_dx_2(kx)
     )
+end
+
+function surface_tension(w,kx,t)
+    return surface_tension(w,kx+w.C*t)
+end
+
+function surface_tension(w,kx,t,c)
+    return surface_tension(w,kx+c*t)
 end
 
 end
